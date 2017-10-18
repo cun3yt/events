a3737572fe04f4dc1488ea129a807827ebe0ebe7
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-10-18 16:06:36 +0300

    Two Action Buttons: Details & Another Event

--- apps/question/urls.py
+++ apps/question/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^fun-event$', views.random_weekend_event, name='random_weekend_event'),
+    url(r'^fun-event-response$', views.fun_event_user_action, name='random_weekend_user_action'),
     url(r'^$', views.event_details, name='event_details'),
 ]
--- apps/question/views.py
+++ apps/question/views.py
@@ -6,6 +6,9 @@ from apps.question.models import Event
 import datetime
 from random import randint
 import time
+import requests
+from urllib.parse import parse_qsl
+import json
 
 
 def this_weekend_definition():
@@ -19,37 +22,83 @@ def this_weekend_definition():
     return "{}T13:00:00Z".format(str(start)), "{}T07:00:00Z".format(str(end))
 
 
-@csrf_exempt
-def random_weekend_event(request):
+def fetch_event(host, id=None):
     start, end = this_weekend_definition()
     query_set = Event.objects.filter(start__gte=start, end__lte=end)
     count = query_set.count()
 
-    random_int = randint(0, count-1)
-    event = query_set[random_int:random_int+1][0]
+    if id is None:
+        random_int = randint(0, count-1)
+        event = query_set[random_int:random_int+1][0]
+    else:
+        event = query_set.filter(id)[0]
 
-    url = "http://{}{}?id={}".format(request.get_host(), reverse('event_details'), event.event_id)
+    url = "http://{}{}?id={}".format(host, reverse('event_details'), event.event_id)
     return JsonResponse({
         "text": "I found a great event for you. I hope you like it!\n*<{}|{}>*".format(url, event.name),
         "username": "Events Bot",
         "mrkdwn": True,
+        "replace_original": False,
         "attachments": [
             {
-                "pretext": (event.description[:140] + '...') if len(event.description) > 140 else event.description,
+                "pretext": (event.description[:140] + "... [<{}|Read More>]".format(url)) if len(event.description) > 140 else event.description,
                 "title": event.name,
                 "fallback": "Required plain-text summary of the attachment.",
                 "color": "#36a64f",
                 "title_link": url,
-                "text": (event.description[:140] + '...') if len(event.description) > 140 else event.description,
+                "text": (event.description[:140] + "... [<{}|Read More>]".format(url)) if len(event.description) > 140 else event.description,
                 "fields": [],
                 "image_url": event.logo,
                 "thumb_url": event.logo,
                 "footer": "Weekend Fun",
                 # "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                "ts": int(time.mktime(event.start.timetuple()))
+                "ts": int(time.mktime(event.start.timetuple())),
+                "callback_id": 'fun_weekend',
+                "actions": [
+                    {
+                        "name": "event_response",
+                        "text": "Event Details",
+                        "type": "button",
+                        "value": "details:{}".format(event.event_id)
+                    },
+                    {
+                        "name": "event_response",
+                        "text": "See Another Event",
+                        "type": "button",
+                        "value": "another_event"
+                    }
+                ]
             }
         ]
     })
+
+
+@csrf_exempt
+def random_weekend_event(request):
+    response = fetch_event(request.get_host())
+    return response
+
+
+@csrf_exempt
+def fun_event_user_action(request):
+    req = json.loads(parse_qsl(request.body.decode("utf-8"))[0][1])
+
+    action_list = req['actions'][0]['value'].split(":")
+    if action_list[0] == "details":
+        url = "http://{}{}?id={}".format(request.get_host(), reverse('event_details'), action_list[1])
+        response_json = {
+            "text": "See the details of event here: {}".format(url),
+            "replace_original": False
+        }
+    elif action_list[0] == "another_event":
+        return fetch_event(request.get_host())
+
+    requests.post(
+        req['response_url'],
+        json=response_json
+    )
+
+    return HttpResponse("")
 
 
 def event_details(request):
